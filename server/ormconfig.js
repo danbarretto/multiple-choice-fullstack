f7e71e0eb262dd3517b9a2a228313bb9134f7708
Daniel Sá Barretto <danielbarretto@usp.br>
2021-02-25 18:37:46 -0300

    Initialize React app

--- server/ormconfig.js
+++ server/ormconfig.js
@@ -1,38 +1,38 @@
 module.exports = [
   {
-    name: "development",
-    type: "mongodb",
-    host:"localhost",
-    port:27017,
-    useUnifiedTopology:true,
-    database:"multiple-choice",
+    name: 'development',
+    type: 'mongodb',
+    host: 'localhost',
+    port: 27017,
+    useUnifiedTopology: true,
+    database: 'multiple-choice',
     synchronize: true,
     logging: true,
-    entities: ["src/entities/**/*.ts"],
-    migrations: ["src/migration/**/*.ts"],
-    subscribers: ["src/subscriber/**/*.ts"],
+    entities: ['src/entities/**/*.ts'],
+    migrations: ['src/migration/**/*.ts'],
+    subscribers: ['src/subscriber/**/*.ts'],
     cli: {
-      entitiesDir: "src/entities",
-      migrationsDir: "src/migration",
-      subscribersDir: "src/subscriber"
-    }
+      entitiesDir: 'src/entities',
+      migrationsDir: 'src/migration',
+      subscribersDir: 'src/subscriber',
+    },
   },
   {
-    name: "production",
-    type: "mongodb",
-    host:"localhost",
-    port:27017,
-    database:"multiple-choice",
+    name: 'production',
+    type: 'mongodb',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: 'multiple-choice',
     url: process.env.DATABASE_URL,
     synchronize: true, // switch this to false once you have the initial tables created and use migrations instead
     logging: false,
-    entities: ["dist/entities/**/*.js"],
-    migrations: ["dist/migration/**/*.js"],
-    subscribers: ["dist/subscriber/**/*.js"],
+    entities: ['dist/entities/**/*.js'],
+    migrations: ['dist/migration/**/*.js'],
+    subscribers: ['dist/subscriber/**/*.js'],
     cli: {
-      entitiesDir: "dist/entities",
-      migrationsDir: "dist/migration",
-      subscribersDir: "dist/subscriber"
-    }
-  }
-];
+      entitiesDir: 'dist/entities',
+      migrationsDir: 'dist/migration',
+      subscribersDir: 'dist/subscriber',
+    },
+  },
+]
